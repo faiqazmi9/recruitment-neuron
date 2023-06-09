5f770f4a46c879165c975b7c942670bc5fb97cc7
faiqazmi9 <faiqazmi9@gmail.com>
2023-06-09 09:02:59 +0700

    menghapus required pada form html dan menambahkan validasi jika email sudah masuk kedalam array

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ var posisiData = ["Jakarta", "Bandung"];
 var lowonganDropdown = document.getElementById("vacancy");
 var posisiDropdown = document.getElementById("posisi");
 
+// Menyimpan array
+var inputData = [];
+
 // Tambahkan pilihan dropdown Lowongan
 lowonganData.forEach(function (lowongan) {
     var option = document.createElement("option");
@@ -22,11 +25,16 @@ posisiData.forEach(function (posisi) {
     posisiDropdown.appendChild(option);
 });
 
+var successMessage = document.createElement("p");
+successMessage.className = "success";
+
 // Menangani event saat form dikirim
 const form = document.getElementById("recruitment-form");
 const dataBody = document.getElementById("dataBody");
 form.addEventListener("submit", function (event) {
     event.preventDefault(); // Mencegah form dikirim secara langsung
+
+    successMessage.innerHTML = "";
 
     // Mengambil nilai input
     const nama = document.getElementById("fullname").value;
@@ -41,44 +49,60 @@ form.addEventListener("submit", function (event) {
         return;
     } else {
 
-        var successMessage = document.createElement("p");
-        successMessage.className = "success";
-        successMessage.textContent = "Lamaran Anda telah terkirim!, Semoga beruntung mendapat panggilan";
-        document.body.appendChild(successMessage);
-
-        Swal.fire({
-            icon: 'success',
-            title: 'Lamaran Terkirim!',
-            showConfirmButton: false,
-            timer: 2000 // Notifikasi akan hilang setelah 2 detik
+        var emailExists = inputData.some(function (item) {
+            return item.email === email;
         });
 
-        // Membuat elemen <tr> dan <td> untuk setiap kolom data
-        const newRow = document.createElement("tr");
-        const namaCell = document.createElement("td");
-        const emailCell = document.createElement("td");
-        const phoneCell = document.createElement("td");
-        const vacancyCell = document.createElement("td");
-        const posisiCell = document.createElement("td");
+        if (emailExists) {
+            // Tampilkan notifikasi email sudah ada
+            Swal.fire({
+                icon: 'error',
+                title: 'Email sudah terdaftar!',
+                showConfirmButton: false,
+                timer: 2000 // Notifikasi akan hilang setelah 2 detik
+            });
+        } else {
 
-        // Mengisi teks pada setiap elemen <td> dengan nilai yang diisi pengguna
-        namaCell.textContent = nama;
-        emailCell.textContent = email;
-        phoneCell.textContent = phone;
-        vacancyCell.textContent = vacancy;
-        posisiCell.textContent = posisi;
+            successMessage.innerHTML = "Terima kasih telah melakukan pengisian, permintaan anda akan kami segera proses!";
+            document.body.appendChild(successMessage);
 
-        // Menambahkan elemen <td> ke dalam elemen <tr>
-        newRow.appendChild(namaCell);
-        newRow.appendChild(emailCell);
-        newRow.appendChild(phoneCell);
-        newRow.appendChild(vacancyCell);
-        newRow.appendChild(posisiCell);
+            inputData.push({ name: nama, email: email, phone: phone, vacancy: vacancy, posisi: posisi });
 
-        // Menambahkan elemen <tr> ke dalam elemen <tbody>
-        dataBody.appendChild(newRow);
+            // Membuat elemen <tr> dan <td> untuk setiap kolom data
+            const newRow = document.createElement("tr");
+            const namaCell = document.createElement("td");
+            const emailCell = document.createElement("td");
+            const phoneCell = document.createElement("td");
+            const vacancyCell = document.createElement("td");
+            const posisiCell = document.createElement("td");
 
-        // Mereset form setelah berhasil ditambahkan ke dalam tabel
-        form.reset();
+            // Mengisi teks pada setiap elemen <td> dengan nilai yang diisi pengguna
+            namaCell.textContent = nama;
+            emailCell.textContent = email;
+            phoneCell.textContent = phone;
+            vacancyCell.textContent = vacancy;
+            posisiCell.textContent = posisi;
+
+            // Menambahkan elemen <td> ke dalam elemen <tr>
+            newRow.appendChild(namaCell);
+            newRow.appendChild(emailCell);
+            newRow.appendChild(phoneCell);
+            newRow.appendChild(vacancyCell);
+            newRow.appendChild(posisiCell);
+
+            // Menambahkan elemen <tr> ke dalam elemen <tbody>
+            dataBody.appendChild(newRow);
+
+            // Notifikasi saat Sukses
+            Swal.fire({
+                icon: 'success',
+                title: 'Lamaran Terkirim!',
+                showConfirmButton: false,
+                timer: 2000 // Notifikasi akan hilang setelah 2 detik
+            });
+
+            // Mereset form setelah berhasil ditambahkan ke dalam tabel
+            form.reset();
+        }
     }
 });
