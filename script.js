19654a0258e877f812d39d5fb7b1c336b8852b58
faiqazmi9 <faiqazmi9@gmail.com>
2023-06-09 10:53:49 +0700

    menambahkan validasi terkait kuota lowongan pekerjaan

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Data untuk dropdown Lowongan
-var lowonganData = ["Database Developer", "System Analyst", "Front End Developer"];
+var lowonganData = ["System Administrator", "Programmer", "Technical Writter"];
 
 // Data untuk dropdown Posisi
 var posisiData = ["Jakarta", "Bandung"];
@@ -28,6 +28,44 @@ posisiData.forEach(function (posisi) {
 var successMessage = document.createElement("p");
 successMessage.className = "success";
 
+var alertMessage = document.createElement("p");
+alertMessage.className = "success";
+
+// Data kuota lowongan
+var kuotaVacancy = {
+    "System Administrator": {
+        kuota: 1,
+        terisi: 0
+    },
+    "Programmer": {
+        kuota: 2,
+        terisi: 0
+    },
+    "Technical Writer": {
+        kuota: 3,
+        terisi: 0
+    }
+};
+
+// Mendapatkan informasi kuota lowongan
+function getLowonganInfo(vacancy) {
+    if (kuotaVacancy.hasOwnProperty(vacancy)) {
+        var lowongan = kuotaVacancy[vacancy];
+        if (lowongan.terisi < lowongan.kuota) {
+            var tersisa = lowongan.kuota - lowongan.terisi;
+            if (tersisa <= 2) {
+                return 'Kuota tersisa untuk ' + vacancy + ' hanya ' + tersisa + ' pendaftar.';
+            } else {
+                return 'Anda dapat memilih lowongan ' + vacancy + '.';
+            }
+        } else {
+            return 'Mohon maaf, rekrutasi untuk ' + vacancy + ' sudah penuh dan tidak dapat dipilih.';
+        }
+    } else {
+        return 'Lowongan tidak tersedia.';
+    }
+}
+
 // Menangani event saat form dikirim
 const form = document.getElementById("recruitment-form");
 const dataBody = document.getElementById("dataBody");
@@ -35,6 +73,7 @@ form.addEventListener("submit", function (event) {
     event.preventDefault(); // Mencegah form dikirim secara langsung
 
     successMessage.innerHTML = "";
+    alertMessage.innerHTML = "";
 
     // Mengambil nilai input
     const nama = document.getElementById("fullname").value;
@@ -43,10 +82,40 @@ form.addEventListener("submit", function (event) {
     const vacancy = document.getElementById("vacancy").value;
     const posisi = document.getElementById("posisi").value;
 
-    // Memvalidasi input
+    // Validasi inputan
     if (nama === "" || email === "" || phone === "" || vacancy === "" || posisi === "") {
-        alert("Harap isi semua kolom!");
-        return;
+        // Tampilkan notifikasi bahwa semua inputan harus diisi
+        Swal.fire({
+            icon: 'error',
+            title: 'Silakan lengkapi semua field!',
+            showConfirmButton: false,
+            timer: 2000 // Notifikasi akan hilang setelah 2 detik
+        });
+    } else if (!isValidEmail(email)) {
+        // Tampilkan notifikasi bahwa email tidak valid
+        Swal.fire({
+            icon: 'error',
+            title: 'Email tidak valid! format email harus benar',
+            showConfirmButton: false,
+            timer: 2000 // Notifikasi akan hilang setelah 2 detik
+        });
+    } else if (!isValidPhoneNumber(phone)) {
+        // Tampilkan notifikasi bahwa nomor telepon tidak valid
+        Swal.fire({
+            icon: 'error',
+            title: 'Nomor telepon tidak valid! harus berawalan 08, minimal 9 karakter dan maksimal 13 karakter',
+            showConfirmButton: false,
+            timer: 2000 // Notifikasi akan hilang setelah 2 detik
+        });
+    } else if (!isValidVacancy(vacancy)) {
+        // Tampilkan notifikasi terkait kuota lowongan
+        var lowonganInfo = getLowonganInfo(vacancy);
+        Swal.fire({
+            icon: 'info',
+            title: lowonganInfo,
+            showConfirmButton: false,
+            timer: 2000 // Notifikasi akan hilang setelah 2 detik
+        });
     } else {
 
         var emailExists = inputData.some(function (item) {
@@ -65,6 +134,13 @@ form.addEventListener("submit", function (event) {
 
             successMessage.innerHTML = "Terima kasih telah melakukan pengisian, permintaan anda akan kami segera proses!";
             document.body.appendChild(successMessage);
+            
+            kuotaVacancy[vacancy].terisi++;
+
+            // Tampilkan notifikasi vacancy menggunakan elemen <p>
+            var lowonganInfo = getLowonganInfo(vacancy);
+            alertMessage.innerHTML = lowonganInfo;
+            document.body.appendChild(alertMessage);
 
             inputData.push({ name: nama, email: email, phone: phone, vacancy: vacancy, posisi: posisi });
 
@@ -106,3 +182,34 @@ form.addEventListener("submit", function (event) {
         }
     }
 });
+
+// Validasi email
+function isValidEmail(email) {
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+// Validasi nomor telepon (hanya angka)
+function isValidPhoneNumber(phone) {
+    var phoneRegex = /^08[0-9]{7,12}$/;
+    return phoneRegex.test(phone);
+}
+
+// Validasi kuota lowongan tersedia
+function isValidVacancy(vacancy) {
+    if (kuotaVacancy.hasOwnProperty(vacancy)) {
+        if (kuotaVacancy[vacancy].terisi < kuotaVacancy[vacancy].kuota) {
+            return true;
+        } else {
+            Swal.fire({
+                icon: 'error',
+                title: 'Mohon maaf, rekrutasi untuk ' + vacancy + ' sudah penuh dan tidak dapat dipilih.',
+                showConfirmButton: false,
+                timer: 2000 // Notifikasi akan hilang setelah 2 detik
+            });
+            return false;
+        }
+    } else {
+        return false;
+    }
+}
